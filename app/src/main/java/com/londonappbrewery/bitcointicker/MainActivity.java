7ae770952c8b43cd90cbb1153f39085ba153380c
Senorita Sundarrajan <senoritasundarrajan>
2017-10-13 20:38:14 +0530

    Challenge Completed!

--- app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/bitcointicker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.londonappbrewery.bitcointicker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -12,16 +13,24 @@ import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.Toast;
 
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import cz.msebera.android.httpclient.entity.mime.Header;
+
+import static com.londonappbrewery.bitcointicker.CoinModel.fromJson;
 
 
 public class MainActivity extends AppCompatActivity {
 
     // Constants:
     // TODO: Create the base URL
-    private final String BASE_URL = "https://apiv2.bitcoin ...";
+    String mCurrency="USD";
+    private final String BASE_URL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"+mCurrency;
 
     // Member Variables:
     TextView mPriceTextView;
@@ -43,34 +52,63 @@ public class MainActivity extends AppCompatActivity {
 
         // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
-
+//        Intent myIntent= new Intent(MainActivity.this,CoinModel.class);
+//        startActivity(myIntent);
         // TODO: Set an OnItemSelected listener on the spinner
+spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+    Log.d("Bitcoin",parent.getItemAtPosition(position).toString());
+        mCurrency=parent.getItemAtPosition(position).toString();
+       // RequestParams params=new RequestParams();
 
+
+        letsDoSomeNetworking(BASE_URL);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+    Log.d("Bitcoin","Nothing has been selected!");
+    }
+});
     }
 
     // TODO: complete the letsDoSomeNetworking() method
     private void letsDoSomeNetworking(String url) {
 
-//        AsyncHttpClient client = new AsyncHttpClient();
-//        client.get(WEATHER_URL, params, new JsonHttpResponseHandler() {
-//
-//            @Override
-//            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-//                // called when response HTTP status is "200 OK"
-//                Log.d("Clima", "JSON: " + response.toString());
-//                WeatherDataModel weatherData = WeatherDataModel.fromJson(response);
-//                updateUI(weatherData);
-//            }
-//
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, Throwable e, JSONObject response) {
-//                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-//                Log.d("Clima", "Request fail! Status code: " + statusCode);
-//                Log.d("Clima", "Fail response: " + response);
-//                Log.e("ERROR", e.toString());
-//                Toast.makeText(WeatherController.this, "Request Failed", Toast.LENGTH_SHORT).show();
-//            }
-//        });
+       AsyncHttpClient client = new AsyncHttpClient();
+       client.get(BASE_URL, new JsonHttpResponseHandler() {
+           @Override
+           public void onStart()
+           {
+
+           }
+
+
+            @Override
+            public void onSuccess(int statusCode, cz.msebera.android.httpclient.Header[] headers, JSONObject response) {
+               // called when response HTTP status is "200 OK"
+
+                CoinModel myCoins=CoinModel.fromJson(response);
+                Log.d("Bitcoin", "JSON: " + response.toString());
+                mPriceTextView.setText(myCoins.price);
+
+            }
+
+            @Override
+            public void onFailure(int statusCode, cz.msebera.android.httpclient.Header[] headers, Throwable e, JSONObject response) {
+                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
+                Log.d("Bitcoin", "Request fail! Status code: " + statusCode);
+                Log.d("Bitcoin", "Fail response: " + response);
+                Log.e("ERROR", e.toString());
+
+            }
+            @Override
+           public void onRetry(int retryNo)
+            {
+
+            }
+        });
 
 
     }
